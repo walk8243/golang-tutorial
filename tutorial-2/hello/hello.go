db63d46ff1277ab1d77fc802fa2adc2b312b5aa6
walk8243 <walk8243@gmail.com>
2021-11-05 14:14:58 +0900

    catch error

--- tutorial-2/hello/hello.go
+++ tutorial-2/hello/hello.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	message1, _ := greetings.Hello("Gladys")
+	fmt.Println(message1)
+
+	// Get an error and exit the program.
+	message1, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message1)
 }
